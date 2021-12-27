c22155c2cbee381c386c635f921ffebb90ee118b
Ilnova <83817034+Ilnova@users.noreply.github.com>
2021-12-27 15:47:06 +0100

    Update
    Per FE completato checkPassword

--- app/controller/controller_profilo.js
+++ app/controller/controller_profilo.js
@@ -74,14 +74,16 @@ if (tipo == "Personale"){
       var oldPassword = req.body.inputOldPassword
       var password = req.body.inputPassword
       var passwordConfirm = req.body.inputConfirmPassword
-      if ((oldPassword == null) || (oldPassword.length <= 7) || (!/^[A-Za-z0-9]+$/.test(oldPassword))) {
+      if ((oldPassword == null) || (oldPassword.length <= 8) || (!(/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,30}$/.test(oldPassword)))
+      ) {
        
         res.json({
           message: "Errore Password vecchia."
       });return;
     }
 
-    if ((password == null) || (password.length <= 7) || (!/^[A-Za-z0-9]+$/.test(password))) {
+    if ((password == null) || (password.length <= 8) || (!(/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,30}$/.test(password)))
+    ) {
         res.json({
           message:"Errore Password."
       });return;
@@ -92,7 +94,52 @@ if (tipo == "Personale"){
           message: "Errore Password di conferma."
       });return;
  }
-  var passwordHashed = hash.hashPassword(password) 
+ 
+ //Non corrisponde a quello sul DB CHECKPASSWORD - vedere come fare find per i tipi
+ 
+var vecio = hash.hashPassword(oldPassword) 
+if (req.session.tipo == "Cliente"){
+Cliente_Model.find({email:mail}, function (err,docs)
+{
+if (err) throw err;
+let hashato=docs[0].password;
+
+  if (checkPassword(hashato.hash, hashato.salt, vecio)==false) {
+    res.json({
+      message: "Password non corrisponde nel DB."
+  });return;
+  }
+  })
+}
+if (req.session.tipo == "Admin"){
+  Admin_Model.find({email:mail}, function (err,docs)
+  {
+  if (err) throw err;
+  let hashato=docs[0].password;
+  
+    if(checkPassword(hashato.hash, hashato.salt, vecio)==false) {
+      res.json({
+        message: "Password non corrisponde nel DB."
+    });return;
+    }
+    })
+  }
+  if (req.session.tipo == "Personale"){
+    Personale_Model.find({email:mail}, function (err,docs)
+    {
+    if (err) throw err;
+    let hashato=docs[0].password;
+    
+      if (checkPassword(hashato.hash, hashato.salt, vecio)==false) {
+        res.json({
+          message: "Password non corrisponde nel DB."
+      });return;
+      }
+      })
+    }
+ 
+ 
+ var passwordHashed = hash.hashPassword(password) 
 
   
    if (req.session.tipo == "Personale"){
