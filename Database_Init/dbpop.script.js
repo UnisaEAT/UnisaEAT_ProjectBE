1dcd3eec85394090c0098d9aecafd00affd80997
Nicola_Cappello <cappellonicola04@gmail.com>
2022-01-03 12:49:28 +0100

    updatePassword corretto

--- Database_Init/dbpop.script.js
+++ Database_Init/dbpop.script.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
-var hash = require('../controller/hash')
+var hash = require('../app/controller/hash')
 // Database URL
 const url = 'mongodb://localhost:27017/UnisaEAT_db'
 // Database name
@@ -19,47 +19,43 @@ function insert() {
             var dbo = db.db(dbName)
 
             const fs = require('fs')
-            const Clientedata = fs.readFileSync('../Database_Init/JSON/Cliente.json')
-          //const personaledata = fs.readFileSync('Database_Init/JSON/dbpop_Personale.js')
-            //const admindata = fs.readFileSync('Database_Init/JSON/dbpop_Admin.js')
+            const clienteData = fs.readFileSync('../Database_Init/JSON/Cliente.json')
+            const personaleData = fs.readFileSync('../Database_Init/JSON/Personale.json')
+            const adminData = fs.readFileSync('../Database_Init/JSON/Admin.json')
 
-            const client = JSON.parse(Clientedata)
-            //const personale = JSON.parse(personaledata)
-           // const admin = JSON.parse(admindata)
+            const client = JSON.parse(clienteData)
+            const personale = JSON.parse(personaleData)
+            const admin = JSON.parse(adminData)
 
             for (var i = 0; client[i] != null; i++) {
                 client[i].password = hash.hashPassword(client[i].password)
             }
-            /*
+            
             for (var j = 0; personale[j] != null; j++) {
                 personale[j].password = hash.hashPassword(personale[j].password)
             }
             for (var k = 0; admin[k] != null; k++) {
                 admin[k].password = hash.hashPassword(admin[k].password)
             }
-        */
-            dbo.collection('clienti').insertMany(client, function(err, result) {
+        
+            dbo.collection('cliente').insertMany(client, function(err, result) {
                 if (err) throw err
-                console.log('abbiamo inserito  ' + result.insertedCount + 'clienti')
-
-                /*
+                console.log('abbiamo inserito  ' + result.insertedCount + 'cliente')
                 dbo.collection('personale').insertMany(personale, function(err, result) {
                     if (err) throw err
                     console.log('abbiamo inserito  ' + result.insertedCount + 'che rigiuardano il personale')
-
                     dbo.collection('admin').insertMany(admin, function(err, result) {
                         if (err) throw err
                         console.log('abbiamo inserito  ' + result.insertedCount + 'admin')
-*/
                         if (err) throw err
                         console.log('Succesfully created the collection UnisaEAT_db.')
                         resolve()
                     })
                 })
             })
-    /*
+    
         })
     })
-    */
+    
 
 }
--- app/models/cliente.js
+++ app/models/cliente.js
@@ -1,23 +1,32 @@
 var ObjectId = require('mongodb').ObjectID;
 module.exports = mongoose => {
-  var schema = mongoose.Schema(
-    {
-      nome: String,
-      cognome: String,
-      password: Object,
-      citta: String,
-      email: String,
-      indirizzo: String,
-      tesserino: ObjectId
-    }
-  );
+    var schema = mongoose.Schema({
+        nome: String,
+        cognome: String,
+        password: Object,
+        citta: String,
+        email: String,
+        indirizzo: String,
+        tesserino: ObjectId,
+        dataDiNascita: String,
+        provinciaDiNascita: String,
+        comuneDiNascita: String,
+        cittadinanza: String,
+        provincia: String,
+        cap: String,
+        telefono: String
+    });
 
-  schema.method("toJSON", function() {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
-  });
+    schema.method("toJSON", function() {
+        const {
+            __v,
+            _id,
+            ...object
+        } = this.toObject();
+        object.id = _id;
+        return object;
+    });
 
-  const Cliente = mongoose.model("cliente", schema, "cliente");
-  return Cliente;
+    const Cliente = mongoose.model("cliente", schema, "cliente");
+    return Cliente;
 };
