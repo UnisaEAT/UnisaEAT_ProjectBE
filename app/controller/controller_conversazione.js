8fed4e8fd20ea88a88a3d7e9af6efe6d4ec625ad
Nicola_Cappello <cappellonicola04@gmail.com>
2022-01-23 14:46:29 +0100

    Update controller_conversazione.js
    aggiunti jsdocs

--- app/controller/controller_conversazione.js
+++ app/controller/controller_conversazione.js
@@ -1,8 +1,12 @@
 const db = require('../models')
 const Conversazione_Model = db.model_conversazione
 
-// crea una nuova conversazione
-// devono essere passati dal fe la mail ed il ruolo di sender e receiver
+/**
+ * Questo metodo crea una nuova conversazione tra un cliente ed un personale adisu
+ * @param {Object} req - L'oggetto request 
+ * @param {Object} res - L'oggetto response
+ * @returns {Object} - Restituisce un oggetto {error:String} in caso di errore, altrimenti restituisce l'oggetto conversazione appena creato
+ */
 exports.create = (req, res) => {
   const sender = req.body.sender
   const receiver = req.body.receiver
@@ -25,6 +29,12 @@ exports.create = (req, res) => {
 
 // restituisce tutte le conversazioni di un utente
 // l'fe passa l'utente {email, ruolo} di cui vuole ottenere tutte le conversazioni
+/**
+ * Questo metodo restituisce tutte le conversazioni di un utente
+ * @param {Object} req - L'oggetto request 
+ * @param {Object} res - L'oggetto response
+ * @returns {(Object|Array)} - Restituisce un oggetto {error:String} in caso di errore, altrimenti restituisce l'array di conversazioni di un utente
+ */
 exports.getConversazioni = (req, res) => {
   const user = req.body.user
 
@@ -40,6 +50,12 @@ exports.getConversazioni = (req, res) => {
 
 // restituisce una conversazione tra due utenti
 // l'fe passa gli utenti {email, ruolo} di cui vuole ottenere la conversazione
+/**
+ * Questo metodo restituisce una conversazione tra due utenti
+ * @param {Object} req - L'oggetto request 
+ * @param {Object} res - L'oggetto response
+ * @returns {Object} - Restituisce un oggetto {error:String} in caso di errore, altrimenti restituisce l'oggetto conversazione
+ */
 exports.getConversazione = (req, res) => {
   const user1 = req.body.user1
   const user2 = req.body.user2
